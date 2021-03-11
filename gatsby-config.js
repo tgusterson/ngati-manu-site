4e47bbaee534c7fdf88726d6237f714a1cdc0aaf
Thom Gusterson <thomgusterson@gmail.com>
2021-03-12 11:35:09 +1300

    Added netlify gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,7 @@ module.exports = {
     //   },
     // },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
