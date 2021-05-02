2d846a7813e4a7c7f2955e77f64ead5d15df1f70
Thom Gusterson <thomgusterson@gmail.com>
2021-05-02 14:04:17 +1200

    trying to fix marae booking

--- functions/utils/emailClient.js
+++ functions/utils/emailClient.js
@@ -1,10 +1,10 @@
 const nodemailer = require('nodemailer');
 module.exports = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
+  // host: 'smtp.gmail.com',
   port: 465,
   ignoreTLS: false,
   secure: false,
-  // service: process.env.EMAIL_SERVICE,
+  service: process.env.EMAIL_SERVICE,
   auth: {
     user: process.env.EMAIL_ADDRESS,
     pass: process.env.EMAIL_PW
